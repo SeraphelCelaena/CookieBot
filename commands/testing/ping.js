d197c28bc3a61148b805dfe1f20b8498a1ff8d8e
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-06 01:51:57 -0600

    First Commit

---
+++ commands/testing/ping.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('mew')
+        .setDescription('Cookie sez mew!'),
+    async execute(interaction) {
+        await interaction.reply('MEW!');
+    }
+}
+
---
+++ index.js
@@ -0,0 +1,64 @@
+// Imports
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Creating the client Class
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+
+// Variables
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // Set a new item in the colletion with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] the command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+// What the bot does when it is on
+client.once(Events.ClientReady, c => {
+    // Says Hi when bot is first launched
+    console.log(`Cookie Sez Mew! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`)
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({content: 'There was an error while executing this command!', ephemeral: true});
+        } else {
+            await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
+        }
+    }
+});
+
+// Discord Login
+client.login(process.env.DISCORD_TOKEN);
