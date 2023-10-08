5fe05c783ff9ec0cbc4958f411b9f0cc8d6530e6
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-07 18:47:21 -0600

    finished deploy-commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,4 @@
-const {REST, routes} = require('discord.js');
+const {REST, Routes} = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 const dotenv = require('dotenv');
@@ -10,5 +10,37 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] the command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
 }
+
+// Construct and prepare an instance of the REST Module.
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+// And deploy your commands!
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        // the put method is used to fully refresh all commands in the guild with the current set.
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID),
+            {body: commands}
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        // and of course, make sure to catch and log any errors
+        console.error(error);
+    }
+})();
