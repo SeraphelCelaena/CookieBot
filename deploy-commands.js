2706b0cf0b610ecc18ed0c3355fba717d8d331d3
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-08 00:30:59 -0600

    can deploy commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,43 +4,47 @@ const path = require('node:path');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const token = process.env.DISCORD_TOKEN;
+const clientID = process.env.CLIENT_ID;
+
 const commands = [];
 // grab all the command files from the commands directory
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            commands.push(command.data.toJSON());
-        } else {
-            console.log(`[WARNING] the command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
+	// Grab all the command files from the commands directory you created earlier
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON());
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 // Construct and prepare an instance of the REST Module.
-const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+const rest = new REST().setToken(token);
 
-// And deploy your commands!
+// and deploy your commands!
 (async () => {
-    try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        // the put method is used to fully refresh all commands in the guild with the current set.
-        const data = await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID),
-            {body: commands}
-        );
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationCommands(clientID),
+			{ body: commands },
+		);
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-        // and of course, make sure to catch and log any errors
-        console.error(error);
-    }
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
 })();
