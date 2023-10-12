68a9ad08edabff9d3acff2ec049c041978c6396a
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-12 02:25:39 -0600

    Updated the Event Handler to be less scuffed

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 
 module.exports = (client, Discord) => {
-	const loadDirectory = (directories) => {
-		const eventFiles = fs.readdirSync(`./events/${directories}`).filter(file => file.endsWith('.js'));
+	const eventsFolder = fs.readdirSync(`./events`);
+
+	for (const folder of eventsFolder) {
+		const eventFiles = fs.readdirSync(`./events/${folder}`).filter(file => file.endsWith('.js'));
 
 		for (const file of eventFiles) {
-			const event = require(`../events/${directories}/${file}`);
+			const event = require(`../events/${folder}/${file}`);
 			const eventName = file.split('.')[0];
 			client.on(eventName, event.bind(null, Discord, client));
 		}
 	}
-
-	const eventsFolder = fs.readdirSync(`./events`);
-	eventsFolder.forEach(event => loadDirectory(event));
 }
