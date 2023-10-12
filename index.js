318d861a09d7d40854cbbc29732b045f50e7caec
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-12 00:43:32 -0600

    refactor code to not use slash commands

--- index.js
+++ index.js
@@ -1,59 +1,23 @@
 // "Imports" idk why import doesnt work
-const fs = require('node:fs');
-const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Creating the client Class
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages
-    ]
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages
+	]
 });
 
-// --- Imports Slash Commands --- \\
+// --- Collections --- \\
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+client.events = new Collection();
 
-for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the colletion with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] the command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
-}
-
-// --- Variables --- \\
-// empty kek
-
-
-// --- BOT FUNCTIONALITY --- \\
-// Import Events
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
-}
-
+['commandHandler', 'eventHandler'].forEach(handler => {
+	require(handler + '.js')(client, Discord);
+})
 
 // --- DISCORD LOGIN --- \\
 client.login(process.env.DISCORD_TOKEN);
