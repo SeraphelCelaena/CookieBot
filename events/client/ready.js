5d977f7a85679eaad5986f84b57c6ec32e8658af
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-12 01:33:16 -0600

    successfully set the activity properly

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
 module.exports = (Discord, client) => {
 	console.log(`Cookie Sez Mew\nLogged in as ${client.user.tag}`);
-	client.user.setActivity("Eating Cheese", {type: "CUSTOM"});
+	client.user.setActivity('Eating Cheese', {type: Discord.ActivityType.Custom});
 }
