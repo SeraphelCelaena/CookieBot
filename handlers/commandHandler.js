6b930edfeea35db979e698af5e6626c56fe7fbc2
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-12 02:25:21 -0600

    Created a Command Handler

---
+++ handlers/commandHandler.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+module.exports = (client, Discord) => {
+	const commandsFolder = fs.readdirSync('./commands');
+
+	for (const folder of commandsFolder) {
+		const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+
+		for (const file of commandFiles) {
+			const command = require(`../commands/${folder}/${file}`);
+			if (command.name) {
+				client.commands.set(command.name, command);
+			} else {
+				continue;
+			}
+		}
+	}
+}
