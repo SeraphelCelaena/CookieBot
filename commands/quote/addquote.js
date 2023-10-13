35e279d5c6318abbaf1da42eac9598d87f2405d3
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-13 00:19:44 -0600

    added log to see what it gives me

--- commands/quote/addquote.js
+++ commands/quote/addquote.js
@@ -4,6 +4,8 @@ module.exports = {
 	permissions: [],
 	description: 'Adds a quote to the database',
 	async execute(client, message, command, arguments, Discord) {
+		console.log(`client: ${client}\nmessage: ${message}\ncommand: ${command}\narguments: ${arguments}\nDiscord: ${Discord}\n`);
+
 		
 	}
 }
