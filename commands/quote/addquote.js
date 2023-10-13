e74d55599e8dd0bb853b6891c8c032639828372e
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-12 22:26:19 -0600

    First commit

---
+++ commands/quote/addquote.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'addquote',
+	aliases: [],
+	permissions: [],
+	description: 'Adds a quote to the database',
+	async execute(client, message, command, arguments, Discord) {
+		
+	}
+}
