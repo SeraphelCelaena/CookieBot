ae7e54c5ccb1091e590dc10aef64627e93cb17f2
SeraphelCelaena <minecraft6401@hotmail.com>
2023-10-17 13:20:34 -0600

    added a quoteModel for quotes

---
+++ models/quoteModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const quoteSchema = new mongoose.Schema({
+    guildID: {type: String, require: true},
+    commandNumber: {type: Number, require: true},
+    quoteContent: {type: String, require: true}
+});
+
+const quoteModel = mongoose.model('quotes', quoteSchema);
+module.exports = quoteModel;
