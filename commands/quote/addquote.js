c58a176558511dd07d3128b971eb1e54390dfd17
SeraphelCelaena <minecraft6401@hotmail.com>
2023-10-17 14:39:35 -0600

    added quote adding, but not working

--- commands/quote/addquote.js
+++ commands/quote/addquote.js
@@ -1,11 +1,24 @@
+const quoteModel = require('../../models/quoteModel.js');
+
 module.exports = {
 	name: 'addquote',
 	aliases: [],
 	permissions: [],
 	description: 'Adds a quote to the database',
-	async execute(client, message, command, arguments, Discord) {
-		console.log(`client: ${client}\nmessage: ${message}\ncommand: ${command}\narguments: ${arguments}\nDiscord: ${Discord}\n`);
+	async execute(client, message, commandName, arguments, Discord) {
+		console.log(`client: ${client}\nmessage: ${message}\ncommand: ${cmds}\narguments: ${args}\nDiscord: ${Discord}\n`);
 
-		
+		let quoteContent = args.join(" ");
+
+		try {
+			let command = await quoteModel.create({
+				guildID: message.guild.id,
+				quoteNumber: await quoteModel.find({guildID: message.guild.id}).estimatedDocumentCount(),
+				quoteContent: quoteContent
+			});
+			quoteModel.save();
+		} catch(error) {
+			console.log(`[Error] addquote: ${error}`);
+		}
 	}
 }
