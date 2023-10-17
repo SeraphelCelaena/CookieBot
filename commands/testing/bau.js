5f21205d8531d8ce3a9a5c4b7e2d39c56b58fd97
Antonio <122330804+Antonio-Solito@users.noreply.github.com>
2023-10-17 14:10:51 -0600

    added test command, bau

---
+++ commands/testing/bau.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "bau",
+  aliases: [],
+  permissions: [],
+  description: "Cookie will speak in his native language",
+  async execute(client, message, command, arguments, Discord) {
+    message.reply(
+      "WOOF BAU! (means hello) <:CookieLoveRed:1131049937505886320>"
+    );
+  },
+};
