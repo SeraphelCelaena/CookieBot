2c2ad12e98423be77afd013483d8d9a8c99b770f
SeraphelCelaena <minecraft6401@hotmail.com>
2023-10-17 14:39:02 -0600

    changed the variable names to be more readable

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 module.exports = async (Discord, client, message) => {
 	const prefix = process.env.PREFIX;
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const cmd = args.shift().toLowerCase();
+	const arguments = message.content.slice(prefix.length).split(/ +/);
+	const commandName = arguments.shift().toLowerCase();
 
-	const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
+	const command = client.commands.get(commandName) || client.commands.find(a => a.aliases && a.aliases.includes(commandName));
 	if (!command) {
 		return message.channel.send("This command does not exist!");
 	}
 
 	try {
-		command.execute(client, message, command, arguments, Discord);
+		command.execute(client, message, commandName, arguments, Discord);
 	} catch (error) {
 		message.reply("There was an error trying to execute this command!");
 		console.log(error);
