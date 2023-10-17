2da93d5f8c56afe6f60f5c7ca5af8452b2b2fc6d
SeraphelCelaena <minecraft6401@hotmail.com>
2023-10-17 14:39:21 -0600

    fixed the execute arguments

--- commands/testing/bau.js
+++ commands/testing/bau.js
@@ -3,7 +3,7 @@ module.exports = {
   aliases: [],
   permissions: [],
   description: "Cookie will speak in his native language",
-  async execute(client, message, command, arguments, Discord) {
+  async execute(client, message, commandName, arguments, Discord) {
     message.reply(
       "WOOF BAU! (means hello) <:CookieLoveRed:1131049937505886320>"
     );
--- commands/testing/mew.js
+++ commands/testing/mew.js
@@ -3,7 +3,7 @@ module.exports = {
 	aliases: [],
 	permissions: [],
 	description: "Cookie Sez Mew!",
-	async execute(client, message, command, arguments, Discord) {
+	async execute(client, message, commandName, arguments, Discord) {
 		message.reply("Mew! <:CookieLoveRed:1131049937505886320>")
 	}
 }
