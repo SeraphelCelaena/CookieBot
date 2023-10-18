cd3c642208b9adc7dac858c585ec605c0965c057
SeraphelCelaena <minecraft6401@hotmail.com>
2023-10-18 09:47:52 -0600

    sez quotenumber

--- commands/quote/addquote.js
+++ commands/quote/addquote.js
@@ -10,16 +10,18 @@ module.exports = {
 			message.channel.send("There is nothing to quote!");
 			return;
 		}
+
+		quoteNumber = await quoteModel.where({guildID: message.guild.id}).countDocuments() + 1;
 		
 		try {
 			let quote = await quoteModel.create({
 				guildID: message.guild.id,
-				quoteNumber: await quoteModel.where({guildID: message.guild.id}).countDocuments() + 1,
+				quoteNumber: quoteNumber,
 				quoteContent: arguments.join(" ")
 			});
 			quote.save();
 
-			message.channel.send(`Added Quote: ${arguments.join(" ")}`);
+			message.channel.send(`Added Quote #${quoteNumber}: ${arguments.join(" ")}`);
 
 		} catch(error) {
 			console.log(`[Error] addquote - ${error}`);
