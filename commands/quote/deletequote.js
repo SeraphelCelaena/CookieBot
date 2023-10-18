2bd82fbdb5f2f079a94fbb6c2c8ea1c38b6d7cb7
SeraphelCelaena <minecraft6401@hotmail.com>
2023-10-18 15:34:15 -0600

    Finished deletequote

--- commands/quote/deletequote.js
+++ commands/quote/deletequote.js
@@ -17,15 +17,25 @@ module.exports = {
 			return;
 		} else if (Number.isInteger(parseInt(quoteNumber)) && Math.sign(quoteNumber) != 1) {
 			message.channel.send("Cannot delete 0/negative quotes!");
-			return
+			return;
 		} else if (typeof quoteNumber == "string" && !Number.isInteger(parseInt(quoteNumber))) {
-			message.channel.send("Don't send string")
-		} else {
-			message.channel.send(`Chomping Quote #${quoteNumber}`)
+			message.channel.send("Don't send string!")
+			return;
 		}
 
 		try {
-			
+			const quoteDelete = await quoteModel.where({guildID: message.guild.id, quoteNumber: quoteNumber}).findOne();
+			console.log(quoteDelete);
+			if (!quoteDelete) return message.channel.send("Invalid quote number");
+			for (let i = 0; i < quoteCount; i++) {
+				const quoteTemp = await quoteModel.where({guildID: message.guild.id, quoteNumber: i + 1}).findOne();
+				if (quoteTemp.quoteNumber == quoteDelete.quoteNumber) {
+					await quoteModel.findOneAndDelete({guildID: quoteTemp.guildID, quoteNumber: quoteTemp.quoteNumber});
+				} else if (quoteTemp.quoteNumber > quoteNumber) {
+					await quoteModel.where({guildID: quoteTemp.guildID, quoteNumber: quoteTemp.quoteNumber}).updateOne({}, {}).set({quoteNumber: quoteTemp.quoteNumber - 1});
+				}
+			}
+
 		} catch(error) {
 			console.log(`[Error] deletequote - ${error}`);
 		}
