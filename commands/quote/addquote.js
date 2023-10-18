cc64135f323c44d61498c032789c3626ca552874
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-17 23:35:38 -0600

    counts correctly and if there is no quote to add nothing happens

--- commands/quote/addquote.js
+++ commands/quote/addquote.js
@@ -7,9 +7,14 @@ module.exports = {
 	description: 'Adds a quote to the database',
 	async execute(client, message, commandName, arguments, Discord) {
 		try {
+			if (arguments.join(" ").trim() == "" || arguments == null) {
+				message.channel.send("There is nothing to quote!");
+				return
+			}
+
 			let quote = await quoteModel.create({
 				guildID: message.guild.id,
-				quoteNumber: await quoteModel.find({guildID: message.guild.id}).estimatedDocumentCount() + 1,
+				quoteNumber: await quoteModel.where({guildID: message.guild.id}).countDocuments() + 1,
 				quoteContent: arguments.join(" ")
 			});
 			quote.save();
