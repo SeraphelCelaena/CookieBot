75b3a1e2a17e286365f0c52705a70d44274e1c11
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-19 00:49:12 -0600

    we can now view a list of commands

--- commands/core/commands.js
+++ commands/core/commands.js
@@ -1,9 +1,31 @@
+const fs = require('fs');
+const {EmbedBuilder} = require('discord.js');
+
 module.exports = {
 	name: 'commands',
-	aliases: ['cmds', 'help'],
+	aliases: ['cmds'],
 	permissions: [],
-	description: "shows list of commands",
+	description: "Shows list of commands",
 	async execute(client, message, commandName, arguments, Discord) {
-		message.channel.send(`${commandName} is Work In Progress`);
+		const commandsFolder = fs.readdirSync('./commands');
+		const commandsEmbed = new EmbedBuilder();
+		
+		for (const folder of commandsFolder) {
+			const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+			let commandsDescription = '';
+
+			for (const file of commandFiles) {
+				const command = require(`../${folder}/${file}`);
+				commandsDescription += `- **${command.name}**: ${command.description}\n`;
+			}
+			const folderName = folder[0].toUpperCase() + folder.toString().slice(1)
+			commandsEmbed.addFields({name: folderName, value: commandsDescription, inline: true});
+		}
+
+		commandsEmbed
+			.setTitle('Commands')
+			.setColor(0xFF1199)
+
+		message.channel.send({embeds: [commandsEmbed]});
 	}
 }
