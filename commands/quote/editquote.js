9004d415ef590222725ce2f590993fa92cc15be1
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-19 00:48:53 -0600

    updated description

--- commands/quote/editquote.js
+++ commands/quote/editquote.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'editquote',
 	aliases: [],
 	permissions: [],
-	description: "shows quotes",
+	description: "Edits a specified quote in the database",
 	async execute(client, message, commandName, arguments, Discord) {
 		message.channel.send(`${commandName} is Work In Progress`);
 	}
