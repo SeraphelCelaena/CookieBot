f1b89663446b734a6c66dace8d9cd81d17524c54
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-19 02:59:41 -0600

    not gunna work on it, more work than it's worth

--- commands/WIP/editquote.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'editquote',
-	aliases: [],
-	permissions: [],
-	description: "Edits a specified quote in the database",
-	async execute(client, message, commandName, arguments, Discord) {
-		message.channel.send(`${commandName} is Work In Progress`);
-	}
-}
