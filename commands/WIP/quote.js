784b527431f7831489b3d108d2d56fef1f60e184
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-19 22:39:58 -0600

    if !quote then sends a random quote

--- commands/WIP/quote.js
+++ commands/WIP/quote.js
@@ -1,9 +1,27 @@
+const quoteModel = require('../../models/quoteModel.js');
+
 module.exports = {
 	name: 'quote',
 	aliases: ['q'],
 	permissions: [],
 	description: "shows quotes",
 	async execute(client, message, commandName, arguments, Discord) {
-		message.channel.send(`${commandName} is Work In Progress`);
+		let quoteSend;
+		const quoteCount = await quoteModel.where({guildID: message.guild.id}).countDocuments();
+		const randomNumber = getRandomInteger(0, quoteCount);
+		console.log(randomNumber);
+
+		if (arguments[0] == 0 || ((arguments == null || arguments.join('').trim() == "") && randomNumber == 0)) {
+			return message.channel.send('#0: "Mew!" -Cookie')
+		}
+		else if (arguments == null || arguments.join('').trim() == "") {
+			quoteSend = await quoteModel.where({guildID: message.guild.id, quoteNumber: randomNumber}).findOne();
+		}
+
+		message.channel.send(`#${quoteSend.quoteNumber}: ${quoteSend.quoteContent}`);
 	}
+}
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
 }
