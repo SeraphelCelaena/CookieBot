df309bb2a2f3bb2dcbbf9ffb1656017bd077de93
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-23 23:58:07 -0600

    edited the descriptions

--- commands/custom-commands/editcustomname.js
+++ commands/custom-commands/editcustomname.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'editcustomname',
 	aliases: ['editcommandname'],
 	permissions: [],
-	description: 'Edits the custom command name.',
+	description: 'Edits a custom command name.',
 	async execute(client, message, commandName, arguments, Discord) {
 		message.channel.send(`${commandName} is planned to be implemented.`);
 	}
--- commands/custom-commands/editcustomresponse.js
+++ commands/custom-commands/editcustomresponse.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'editcustomresponse',
 	aliases: ['editcommandresponse'],
 	permissions: [],
-	description: 'Edits a custom command.',
+	description: 'Edits a custom command reponse.',
 	async execute(client, message, commandName, arguments, Discord) {
 		message.channel.send(`${commandName} is planned to be implemented.`);
 	}
