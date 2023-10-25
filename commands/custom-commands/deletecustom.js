0b8c0978a42181963b744a0198eda96639f8300e
SeraphelCelaena <minecraft6401@hotmail.com>
2023-10-25 09:10:52 -0600

    completed deletecustom.js

--- commands/custom-commands/deletecustom.js
+++ commands/custom-commands/deletecustom.js
@@ -1,9 +1,30 @@
+// imports
+const customCommandModel = require('../../models/customCommandModel.js');
+
 module.exports = {
 	name: 'deletecustom',
 	aliases: ['deletecommand', 'removecustom', 'removecommand', 'delcustom', 'delcommand'],
 	permissions: [],
-	description: '[PLANNED] Deletes custom command.',
+	description: 'Deletes custom command.',
 	async execute(client, message, commandName, arguments, Discord) {
-		message.channel.send(`${commandName} is planned to be implemented.`);
+		// variables
+		const commandNameRemove = arguments[0];
+
+		// checks and balances
+		if (!commandNameRemove) return message.channel.send("Specify a command to remove!");
+
+
+		// tries to find command in custom command database, if not yells at user
+		const commandRemove = await customCommandModel.where({guildID: message.guild.id, customCommandName: commandNameRemove.toLowerCase()}).findOne();
+		if (!commandRemove) return message.channel.send('Could not find command to delete');
+
+		// tries to delete command
+		try {
+			await customCommandModel.findOneAndDelete({guildID: commandRemove.guildID, customCommandName: commandRemove.customCommandName});
+			message.channel.send(`Deleted command: ${commandRemove.customCommandName}`);
+		}
+		catch(error) {
+			console.log(`[ERROR] deletecustom - ${error}`);
+		}
 	}
 }
