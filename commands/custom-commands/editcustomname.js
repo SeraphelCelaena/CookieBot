5df43ff9a1caf863efc9f42c078c2075838ce0d5
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-10-28 02:20:18 -0600

    moved the edit customs into proper folder

--- commands/custom-commands/editcustomname.js
+++ commands/custom-commands/editcustomname.js
@@ -3,25 +3,25 @@ const customCommandModel = require("../../models/customCommandModel.js");
 require('dotenv').config();
 
 module.exports = {
-	name: 'editcustomresponse',
-	aliases: ['editcommandresponse'],
+	name: 'editcustomname',
+	aliases: ['editcommandname'],
 	permissions: [],
-	description: '[PLANNED] Edits a custom command response.',
+	description: 'Edits a custom command name.',
 	async execute(client, message, commandName, arguments, Discord) {
 		// variabled
 		const commandNameEdit = arguments[0];
-		const commandResponseEditContent = arguments.slice(1).join(" ");
+		const commandNameEditContent = arguments[1];
 
 		// if sends nothin then gives warn
 		if (commandNameEdit == null) {
 			return message.channel.send("Specify a command to edit!");
 		}
 		// if sends only the command name
-		else if (!commandResponseEditContent) {
+		else if (!commandNameEditContent) {
 			return message.channel.send("Specify a response for the command!");
 		}
 		// if makes edit too long
-		else if (commandResponseEditContent.length > process.env.MAX_QUOTE_LEGNTH) {
+		else if (commandNameEditContent.length > process.env.MAX_QUOTE_LEGNTH) {
 			return message.channel.send(`Too long: String must be shorter than ${process.env.MAX_QUOTE_LEGNTH}, yours is ${commandNameEdit.length}`);
 		}
 
@@ -31,10 +31,10 @@ module.exports = {
 			if (!commandEdit) return message.channel.send("Enter a valid command!");
 
 			// edits command
-			await customCommandModel.where({guildID: commandEdit.guildID, customCommandName: commandEdit.customCommandName}).updateOne({}, {}).set({customCommandResponse: commandResponseEditContent});
+			await customCommandModel.where({guildID: commandEdit.guildID, customCommandName: commandEdit.customCommandName}).updateOne({}, {}).set({customCommandName: commandNameEditContent});
 
 			// success message
-			message.channel.send(`Edited command ${commandEdit.customCommandName} => ${commandResponseEditContent}`);
+			message.channel.send(`Edited command ${commandEdit.customCommandName} => ${commandNameEditContent}`);
 		}
 		catch(error) {
 			console.log(`[ERROR] editcustomname - ${error}`);
--- commands/custom-commands/editcustomresponse.js
+++ commands/custom-commands/editcustomresponse.js
@@ -3,25 +3,25 @@ const customCommandModel = require("../../models/customCommandModel.js");
 require('dotenv').config();
 
 module.exports = {
-	name: 'editcustomname',
-	aliases: ['editcommandname'],
+	name: 'editcustomresponse',
+	aliases: ['editcommandresponse'],
 	permissions: [],
-	description: '[PLANNED] Edits a custom command name.',
+	description: 'Edits a custom command response.',
 	async execute(client, message, commandName, arguments, Discord) {
 		// variabled
 		const commandNameEdit = arguments[0];
-		const commandNameEditContent = arguments[1];
+		const commandResponseEditContent = arguments.slice(1).join(" ");
 
 		// if sends nothin then gives warn
 		if (commandNameEdit == null) {
 			return message.channel.send("Specify a command to edit!");
 		}
 		// if sends only the command name
-		else if (!commandNameEditContent) {
+		else if (!commandResponseEditContent) {
 			return message.channel.send("Specify a response for the command!");
 		}
 		// if makes edit too long
-		else if (commandNameEditContent.length > process.env.MAX_QUOTE_LEGNTH) {
+		else if (commandResponseEditContent.length > process.env.MAX_QUOTE_LEGNTH) {
 			return message.channel.send(`Too long: String must be shorter than ${process.env.MAX_QUOTE_LEGNTH}, yours is ${commandNameEdit.length}`);
 		}
 
@@ -31,10 +31,10 @@ module.exports = {
 			if (!commandEdit) return message.channel.send("Enter a valid command!");
 
 			// edits command
-			await customCommandModel.where({guildID: commandEdit.guildID, customCommandName: commandEdit.customCommandName}).updateOne({}, {}).set({customCommandName: commandNameEditContent});
+			await customCommandModel.where({guildID: commandEdit.guildID, customCommandName: commandEdit.customCommandName}).updateOne({}, {}).set({customCommandResponse: commandResponseEditContent});
 
 			// success message
-			message.channel.send(`Edited command ${commandEdit.customCommandName} => ${commandNameEditContent}`);
+			message.channel.send(`Edited command ${commandEdit.customCommandName} => ${commandResponseEditContent}`);
 		}
 		catch(error) {
 			console.log(`[ERROR] editcustomname - ${error}`);
