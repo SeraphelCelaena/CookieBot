bdb2e53cc4b98a6ebecb157e74246afbc2585c8f
SeraphelCelaena <minecraft6401@hotmail.com>
2023-11-06 11:41:20 -0700

    added the fullcmds alias that does smthn

--- commands/core/commands.js
+++ commands/core/commands.js
@@ -5,7 +5,7 @@ const {EmbedBuilder} = require('discord.js');
 // exports commands command
 module.exports = {
 	name: 'commands',
-	aliases: ['cmds'],
+	aliases: ['cmds', 'fullcmds'],
 	permissions: [],
 	description: "Shows list of commands",
 	async execute(client, message, commandName, arguments, Discord) {
@@ -19,15 +19,17 @@ module.exports = {
 			const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
 			let commandsDescription = '';
 
-			if (!commandFiles.length == 0 && !commandFiles.includes('dummy.js')) {
+			if (!commandFiles.length == 0 && (!commandFiles.includes('dummy.js') || commandName == 'fullcmds')) {
 				// loops through the files
 				for (const file of commandFiles) {
+					if (file == 'dummy.js') continue; // skips dummy.js (if it exists)
 					const command = require(`../${folder}/${file}`);
 					commandsDescription += `- **${command.name}**: ${command.description}\n`;
 				}
 
 				// adds a field to the embed
-				const folderName = folder[0].toUpperCase() + folder.toString().slice(1)
+				const folderName = folder[0].toUpperCase() + folder.toString().slice(1);
+				if (commandsDescription == '') commandsDescription = 'No commands';
 				commandsEmbed.addFields({name: folderName, value: commandsDescription, inline: true});
 			}
 		}
