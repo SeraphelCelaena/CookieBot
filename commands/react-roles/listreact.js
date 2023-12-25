02b2403f3979ece892ecf3adb58c25317bbcda74
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-12-24 20:21:50 -0700

    removed react roles as i think it is too complicated for me to implement

--- commands/react-roles/listreact.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'listreact',
-	aliases: [],
-	permissions: [],
-	description: 'does nothing',
-	async execute(client, message, commandName, arguments, Discord) {
-		message.channel.send(`${commandName} is planned to be implemented.`);
-	}
-}
