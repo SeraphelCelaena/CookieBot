68d52298417875f063af8328301fee7af3ed16d9
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2024-04-09 11:46:04 -0600

    upgrading help command

--- commands/core/help.js
+++ commands/core/help.js
@@ -1,6 +1,7 @@
 // imports
 const fs = require('fs');
 const {EmbedBuilder} = require('discord.js');
+require('dotenv').config();
 
 module.exports = {
 	name: 'help',
@@ -13,9 +14,12 @@ module.exports = {
 		const commandsFolder = fs.readdirSync('./commands');
 		const helpEmbed = new EmbedBuilder();
 		const command = arguments[0];
+		let helpHeader = "";
 		let embedText = "";
 
 		if (!command) return message.channel.send("Specify a command you need help with!");
+
+		helpHeader = `Command Info: ${process.env.PREFIX}${client.commands.get(command).name}`;
 
 		// loops through aliases
 		embedText += `**Aliases**: `;
@@ -24,13 +28,13 @@ module.exports = {
 		}
 
 		// adds stuph to the embed text
-		embedText += `\n**Description:**\n${client.commands.get(command).description}`;
+		embedText += `\n**Description:**\n${client.commands.get(command).description}\n`;
 		embedText += `\n**Usage:**\n\`${client.commands.get(command).help}\``;
 
 		// makes the embed
 		helpEmbed
 			.setColor(0xFF1199)
-			.addFields({name: client.commands.get(command).name, value: embedText});
+			.addFields({name: helpHeader, value: embedText});
 
 		// sends embed
 		message.channel.send({embeds: [helpEmbed]});
