5416de96c3f4167ea93a1e4fc5d2ebcbec9d1e89
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2024-04-09 23:31:51 -0600

    can search by aliases

--- commands/core/help.js
+++ commands/core/help.js
@@ -1,5 +1,4 @@
 // imports
-const fs = require('fs');
 const {EmbedBuilder} = require('discord.js');
 require('dotenv').config();
 
@@ -11,7 +10,6 @@ module.exports = {
 	description: 'displays the command and how to use it.',
 	async execute(client, message, commandName, arguments, Discord) {
 		// variables
-		const commandsFolder = fs.readdirSync('./commands');
 		const helpEmbed = new EmbedBuilder();
 		const command = arguments[0];
 		let helpHeader = "";
@@ -19,17 +17,31 @@ module.exports = {
 
 		if (!command) return message.channel.send("Specify a command you need help with!");
 
-		helpHeader = `Command Info: ${process.env.PREFIX}${client.commands.get(command).name}`;
+		// checks if the command exists
+		let commandSearch = client.commands.get(command);
+		if (!commandSearch) {
+			client.commands.map((cmd) => {
+				if (cmd.aliases.includes(command)) {
+					commandSearch = cmd;
+					return;
+				}
+			});
+		}
+
+		// if the command still doesn't exist
+		if (!commandSearch) return message.channel.send("That command does not exist!");
+
+		helpHeader = `Command Info: ${process.env.PREFIX}${commandSearch.name}`;
 
 		// loops through aliases
 		embedText += `**Aliases**: `;
-		for (var alias of client.commands.get(command).aliases) {
+		for (var alias of commandSearch.aliases) {
 			embedText += `\`${alias}\` `;
 		}
 
 		// adds stuph to the embed text
-		embedText += `\n**Description:**\n${client.commands.get(command).description}\n`;
-		embedText += `\n**Usage:**\n\`${client.commands.get(command).help}\``;
+		embedText += `\n**Description:**\n${commandSearch.description}\n`;
+		embedText += `\n**Usage:**\n\`${commandSearch.help}\``;
 
 		// makes the embed
 		helpEmbed
