28b52f6708957e9bd549c9920f215bdf3741a108
SeraphelCelaena <minecraft6401@hotmail.com>
2024-09-10 14:13:27 -0600

    more crashproof

--- commands/core/help.js
+++ commands/core/help.js
@@ -17,20 +17,30 @@ module.exports = {
 
 		if (!command) return message.channel.send("Specify a command you need help with!");
 
+		let foundCommand = null;
+		client.commands.forEach(cmd => {
+			if (cmd.name == command || cmd.aliases.includes(command)) {
+				foundCommand = cmd;
+			}
+		})
+
+		if (!foundCommand) return message.channel.send("Command not found!");
+
 		// loops through aliases
 		embedText += `**Aliases**: `;
-		for (var alias of client.commands.get(command).aliases) {
+		for (var alias of foundCommand.aliases) {
 			embedText += `\`${alias}\` `;
 		}
 
 		// adds stuph to the embed text
-		embedText += `\n**Description:**\n${client.commands.get(command).description}`;
-		embedText += `\n**Usage:**\n\`${client.commands.get(command).help}\``;
+		if (foundCommand.description) embedText += `\n**Description:**\n${foundCommand.description}`;
+		if (foundCommand.help) embedText += `\n**Usage:**\n\`${foundCommand.help}\``;
+		if (foundCommand.example) embedText += `\n**Example:**\n\`${foundCommand.example}\``;
 
 		// makes the embed
 		helpEmbed
 			.setColor(0xFF1199)
-			.addFields({name: client.commands.get(command).name, value: embedText});
+			.addFields({name: foundCommand.name, value: embedText});
 
 		// sends embed
 		message.channel.send({embeds: [helpEmbed]});
